00ed45c842ffa79acce7b895e9ec79ab6e715be5
Aniket Pandharabale <aniketicloud@gmail.com>
2023-11-23 12:16:49 +0000

    Streaming content with React Server Components

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,7 +1,32 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
 export default function SnippetCreate() {
+  async function createSnippet(formData: FormData) {
+    // this needs to be a server function
+    "use server";
+
+    // check the user inputs for validation
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+
+    // redirect the user to root route
+    redirect("/");
+  }
+
   return (
     <div className="pt-32 absolute inset-0 bg-gradient-to-b from-amber-200">
-      <form className="container mx-auto px-12">
+      <form className="container mx-auto px-12" action={createSnippet}>
         <h3 className="font-bold my-3">Create a Snippet</h3>
         <div className="flex flex-col gap-4">
           <div className="flex gap-4">
