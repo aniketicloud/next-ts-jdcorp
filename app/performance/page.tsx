25756c6f4d89a17356e6569a553d14c5efb5861d
Aniket Pandharabale <aniketicloud@gmail.com>
2023-11-23 07:20:50 +0000

    header style, images for other pages

--- app/performance/page.tsx
+++ app/performance/page.tsx
@@ -1,3 +1,12 @@
+import Hero from "@/components/hero";
+import performanceImg from "@/public/performance.jpg";
+
 export default function PerformancePage() {
-    return <div>Perfomrance Page</div>
+  return (
+    <Hero
+      imgAlt="welding"
+      imgData={performanceImg}
+      title="We serve high performance applications"
+    />
+  );
 }
